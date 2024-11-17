5f06b600fc9d68ad3c36c3e83acb50b14195cb96
HungDevPy <alonedeveloper203@gmail.com>
2024-11-17 23:32:41 +0700

    update card

--- src/components/Infor/Infor.jsx
+++ src/components/Infor/Infor.jsx
@@ -1,13 +1,14 @@
 import { dataInfor } from "@components/Infor/constants"
 import InforCard from "@components/Infor/InforCard/InforCard"
 import MainLayout from "@components/Layout/Layout"
-
+import styles from "./styles.module.scss"
 function Infor() {
+  const {container} = styles;
   return<div>
     <MainLayout>
-        <div>
+        <div className={container}>
             {dataInfor.map((item) =>{
-                return <InforCard/>;
+                return <InforCard title={item.title} description={item.description} src={item.src}/>;
             })}
         </div>
     </MainLayout>
--- src/components/Infor/InforCard/InforCard.jsx
+++ src/components/Infor/InforCard/InforCard.jsx
@@ -1,6 +1,17 @@
+import styles from '../styles.module.scss';
 
-function InforCard() {
-  return <div>Infor </div>
+function InforCard({title,description,src}) {
+
+  const { containerCard,containerContent,des,content } = styles;
+
+
+  return <div className={containerCard}>
+         <img src={src} alt="" width={40} height={40} />
+         <div className={containerContent}>
+            <div className={content}>{title}</div>
+            <div className={des}>{description}</div>
+         </div>
+    </div>
 }
 
 export default InforCard
